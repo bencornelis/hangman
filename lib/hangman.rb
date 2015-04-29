45e0c312cce4cbb5ff4926c2925d6780c22c3c21
Ben & Adam McFadden <student@epicodus.com>
2015-04-29 14:48:32 -0700

    Add check_letter method to change displayed word based on correctness.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,10 +5,11 @@ class Hangman
 
   define_method(:initialize) do
     @word = @@words[rand(@@words.length())]
+    @word_array = @word.split('')
     @hidden_word = '_ '*(@word.length())
     @hidden_word = @hidden_word[0...-1]
-
-    # @incorrect_letters = []
+    @hidden_word_array = @hidden_word.split(' ')
+    @incorrect_letters = []
   end
 
   define_method(:word) do
@@ -18,5 +19,25 @@ class Hangman
   define_method(:hidden_word) do
     @hidden_word
   end
+
+  define_method(:incorrect_letters) do
+    @incorrect_letters
+  end
+
+  define_method(:check_letter) do |letter|
+    correct_letter = @word.split('').include?(letter)
+    unless correct_letter
+      @incorrect_letters.push(letter)
+    end
+    @word_array.each() do |indexing_letter|
+      if indexing_letter == letter
+        i = @word_array.index(indexing_letter)
+        @hidden_word_array[i] = letter
+      end
+    end
+    @hidden_word = @hidden_word_array.join(' ')
+
+  end
+
 
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -8,4 +8,18 @@ describe('Hangman') do
       expect(test_hangman.word()).to(eq('epic'))
     end
   end
+
+  describe('#check_letter') do
+    it("adds the letter to incorrect_letters if the letter isn't in the word") do
+      test_hangman = Hangman.new()
+      test_hangman.check_letter('a')
+      expect(test_hangman.incorrect_letters()).to(eq(['a']))
+    end
+
+    it("changes displayed word if the letter is correct") do
+      test_hangman = Hangman.new()
+      test_hangman.check_letter('e')
+      expect(test_hangman.hidden_word()).to(eq('e _ _ _'))
+    end
+  end
 end
